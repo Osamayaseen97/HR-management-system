5ae0298cb4453be823f9b3ec342dc5e8a778ca8f
osamayaseen97 <osamayaseen1997@gmail.com>
2023-05-28 15:09:52 +0000

    adding employees cards

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function Employee(employeeID, fullName, department, level, imageUrl) {
 Employee.prototype.generateEmployeeID = function () {
   return Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 };
-// Method to calculating the salary
+
 Employee.prototype.calculateSalary = function () {
   let minSalary, maxSalary;
 
@@ -37,36 +37,70 @@ Employee.prototype.calculateSalary = function () {
   const netSalary = randomSalary - (randomSalary * 7.5) / 100;
 
   return netSalary;
-  
 };
 
-
 Employee.prototype.render = function () {
-  document.write(`Employee name: ${this.fullName}<br>`);
-  document.write(`Department: ${this.department}<br>`);
-  document.write(`Employee salary: ${this.salary}<br><br>`);
+  let cardContainer = document.getElementById('card-container');
+  let card = document.createElement('div');
+  card.classList.add('card');
+
+  let image = document.createElement('img');
+  image.src = this.imageUrl;
+  image.alt = this.fullName;
+
+  let infoContainer = document.createElement('div');
+  infoContainer.classList.add('info-container');
+
+  let id = document.createElement('p');
+  id.textContent = `Employee ID: ${this.employeeID}`;
+
+  let name = document.createElement('p');
+  name.textContent = `Name: ${this.fullName}`;
+
+  let department = document.createElement('p');
+  department.textContent = `Department: ${this.department}`;
+
+  let level = document.createElement('p');
+  level.textContent = `Level: ${this.level}`;
+
+  let salary = document.createElement('p');
+  salary.textContent = `Salary: ${this.salary}`;
+
+  infoContainer.appendChild(id);
+  infoContainer.appendChild(name);
+  infoContainer.appendChild(department);
+  infoContainer.appendChild(level);
+  infoContainer.appendChild(salary);
+
+  card.appendChild(image);
+  card.appendChild(infoContainer);
+
+  cardContainer.appendChild(card);
 };
 
 // Create employee instances
-let ghazi = new Employee(null, 'Ghazi Samer', 'Administration', 'Senior','https://example.com/ghazi.jpg');
-let lana = new Employee(null, 'Lana Ali', 'Finance', 'Senior','https://example.com/lana.jpg');
-let tamara = new Employee(null, 'Tamara Ayoub', 'Administration', 'Senior','https://example.com/safi.jpg');
-let safi = new Employee(null, 'Safi Walid', 'Development', 'Mid-Senior', 'https://example.com/omar.jpg');
-let rana = new Employee(null, 'Rana Saleh', 'Development', 'Junior', 'https://example.com/rana.jpg');
-let hadi = new Employee(null, 'Hadi Ahmad', 'Finance', 'Mid-Senior','https://example.com/hadi.jpg');
+let ghazi = new Employee(null, 'Ghazi Samer', 'Administration', 'Senior', 'assets/Ghazi.jpg');
+let lana = new Employee(null, 'Lana Ali', 'Finance', 'Senior', 'assets/Lana.jpg');
+let tamara = new Employee(null, 'Tamara Ayoub', 'Administration', 'Senior', 'assets/Tamara.jpg');
+let safi = new Employee(null, 'Safi Walid', 'Development', 'Mid-Senior', 'assets/Safi.jpg');
+let rana = new Employee(null, 'Rana Saleh', 'Development', 'Junior', 'assets/Rana.jpg');
+let hadi = new Employee(null, 'Hadi Ahmad', 'Finance', 'Mid-Senior', 'assets/Hadi.jpg');
+let omar = new Employee(null, 'Omar Khalil', 'Development', 'Mid-Senior', 'assets/Omar.jpg');
 
-//  Method to Generate a unique four digits employee ID
+// Generate unique four digits employee IDs
 ghazi.employeeID = ghazi.generateEmployeeID();
 lana.employeeID = lana.generateEmployeeID();
 tamara.employeeID = tamara.generateEmployeeID();
 safi.employeeID = safi.generateEmployeeID();
 rana.employeeID = rana.generateEmployeeID();
 hadi.employeeID = hadi.generateEmployeeID();
+omar.employeeID = omar.generateEmployeeID();
 
-// Render employee informations
+// Render employee information
 ghazi.render();
 lana.render();
 tamara.render();
 safi.render();
 rana.render();
 hadi.render();
+omar.render();
